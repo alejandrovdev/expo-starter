afa5cad8d0a0422e42075e5a1eddafa1cbb946bd
Alejandro Villanueva <connect@alejandrov.dev>
2025-05-09 13:54:20 -0600

    chore: update Tailwind CSS content paths to include shared directory and remove redundant entries

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,7 @@ const { hairlineWidth } = require('nativewind/theme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: [
-    './app/**/*.{js,jsx,ts,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}',
-    './lib/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './shared/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
     extend: {
